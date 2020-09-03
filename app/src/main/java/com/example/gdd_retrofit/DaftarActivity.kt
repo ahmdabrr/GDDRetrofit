d16102bc38f20d8fbdfaf7b97c24f461d9427446
VJohanWisnuaji <wisnuaji.johan@gmail.com>
2020-09-03 12:42:20 +0700

    halaman daftar

---
+++ app/src/main/java/com/example/gdd_retrofit/DaftarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gdd_retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DaftarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar)
+    }
+}
